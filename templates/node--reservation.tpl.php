6768fe1651b0306a65617502340f4abc60948e0b
Arshad Chummun <arshad@chummun.com>
2014-04-14 22:04:00 +0400

    Updated message on status update

--- templates/node--reservation.tpl.php
+++ templates/node--reservation.tpl.php
@@ -7,6 +7,7 @@
  * This is the template used in the reservation emails.
  */
 ?>
+<h4><?php print t('Reservation Details'); ?></h4>
 <?php foreach (element_children($content) as $field): ?>
 <?php if (isset($content[$field]['#title']) && isset($content[$field][0]['#markup'])): ?>
 <strong><?php print $content[$field]['#title']; ?>:</strong> <?php print $content[$field][0]['#markup']; ?><br /><br />
